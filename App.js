55c14778aa78dc572dcd42be0b9a6a90bd0103d6
Harsonica <72625830+Harsonica@users.noreply.github.com>
2021-05-30 22:57:36 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottonTabNavigator} from 'react-navigation-tabs';
+import WriteStoryScreen from './screens/WriteStoryScreen';
+import ReadStoryScreen from './screens/ReadStoryScreen';
+import db from '../.config';
+import firebase from 'firebase'; 
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <AppContainer />
+      
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  ReadStory: {screen: ReadStoryScreen},
+  WriteStory: {screen: WriteStoryScreen},
+},
+
+{
+  defaultNavigationOptions:({navigation})=>({
+    tabBarIcon:()=>{
+      const routeName = navigation.state.routeName
+      if(routeName === "ReadStory"){
+        return(
+          <Image source = {require("./assets/read.png")}
+          style = {{width:40, height:40}}>
+
+          </Image>
+        )
+      }
+      else if(routeName === "Write"){
+        return(
+          <Image source = {require("./assets/write.png")}
+          style = {{width: 40, height:40}}>
+
+          </Image>
+        )
+      }
+    }
+  })
+}
+);
+
+const AppContainer =  createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
